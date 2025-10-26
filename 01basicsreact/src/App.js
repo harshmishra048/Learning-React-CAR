a02dc157f8666dd85e8aa777c4d0416272a596d5
mishraharsh4353 <mishraharsh4353@gmail.com>
2025-10-27 02:28:04 +0530

    basics of git

---
+++ 01basicsreact/src/App.js
@@ -0,0 +1,12 @@
+import Chai from './Chai';
+function App() {
+  return (
+    // always use the fragments for the multiple components and make sure that the function name must be capitalize for not getting the error. and its a good practice
+    <>
+      <h1>Hello world</h1>
+      <Chai/>
+    </>
+  );
+}
+
+export default App;
---
+++ 01basicsreact/src/Chai.js
@@ -0,0 +1,8 @@
+
+export default function Chai() {
+  return (
+    <div>
+      <h1>Hello welcome to chai aur code</h1>
+    </div>
+  )
+}
